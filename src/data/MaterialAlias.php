a6df5f71891348b7e538e7ef72e1046f71da5ed8
jasonwynn10 <jasonwynn10@me.com>
2022-09-24 19:18:04 -0400

    Fixed type issues

--- src/data/MaterialAlias.php
+++ src/data/MaterialAlias.php
@@ -53,7 +53,7 @@ final class MaterialAlias implements IteratorAggregate{
 		if($block instanceof VeinBlock){
 			return $this->blocks->contains($block);
 		}elseif($block instanceof BlockIdentifier){
-			return count(array_filter($this->blocks, static fn(VeinBlock $b) => $b->encapsulates($block))) > 0;
+			return count(array_filter($this->blocks->toArray(), static fn(VeinBlock $b) => $b->encapsulates($block))) > 0;
 		}
 		return !$block instanceof Air && $this->isAliased($block->getIdInfo());
 	}
@@ -68,8 +68,8 @@ final class MaterialAlias implements IteratorAggregate{
 		return $this->blocks->toArray();
 	}
 
-	public function getIterator() : array {
-		return $this->blocks->toArray();
+	public function getIterator() : \Traversable {
+		return $this->blocks;
 	}
 
 	public function __clone() {
